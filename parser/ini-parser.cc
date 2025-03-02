afde98021156f40e06adc80ca69ff73cdeb57ab3
XBambusakX <XBambusakX@gmail.com>
2025-03-02 20:25:24 +0100

    fixed whitespace errors

--- parser/ini-parser.cc
+++ parser/ini-parser.cc
@@ -2,39 +2,50 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <algorithm>
+#include <cctype>
 
 #include "ini-parser.hpp"
 
+std::string trim(const std::string& str) {
+    auto first = str.find_first_not_of(" \t\n\r\f\v");
+    if (first == std::string::npos) {
+        return "";
+    }
+    auto last = str.find_last_not_of(" \t\n\r\f\v");
+    return str.substr(first, (last - first + 1));
+}
+
 iniParser::iniParser(const std::string& filename) : filename(filename), loaded(false) {
     std::ifstream file(filename);
-    if(!file.is_open()) {
+    if (!file.is_open()) {
         std::cerr << "Error: could not open file " << filename << std::endl;
         return;
     }
 
     std::string line;
     std::string currentSection;
-    while(std::getline(file, line)) {
-        if(line.empty() || line[0] == ';' || line[0] == '#' || (line[0] == '/' && line[1] == '/')) {
+    while (std::getline(file, line)) {
+        if (line.empty() || line[0] == ';' || line[0] == '#' || (line[0] == '/' && line[1] == '/')) {
             continue;
         }
 
-        if(line[0] == '[' && line[line.size() - 1] == ']') {
-            currentSection = line.substr(1, line.size() - 2);
+        if (line[0] == '[' && line[line.size() - 1] == ']') {
+            currentSection = trim(line.substr(1, line.size() - 2));
             continue;
         }
 
         auto delimiter = line.find('=');
-        if(delimiter == std::string::npos) {
+        if (delimiter == std::string::npos) {
             continue;
         }
 
-        auto key = line.substr(0, delimiter);
-        auto value = line.substr(delimiter + 1);
+        auto key = trim(line.substr(0, delimiter));
+        auto value = trim(line.substr(delimiter + 1));
         data[currentSection][key] = value;
     }
-  file.close();
-  loaded = true;
+    file.close();
+    loaded = true;
 }
   iniParser::~iniParser() {
 }
